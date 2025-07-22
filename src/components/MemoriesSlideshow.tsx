2e6d49525745f03dddd6759201acaae17c4b6dcb
Anjan-Kumar-Sahoo <anjankumarsahoo1097@gmail.com>
2025-07-23 01:39:12 +0530

    Making changes for vercel

--- src/components/MemoriesSlideshow.tsx
+++ src/components/MemoriesSlideshow.tsx
@@ -3,16 +3,16 @@ import SmoothSlideshow from './SmoothSlideshow';
 
 const MemoriesSlideshow: React.FC = () => {
   const memoryImages = [
-    { src: "/src/assets/images/memories/m1.jpg", label: "Memory 1" },
-    { src: "/src/assets/images/memories/m2.jpg", label: "Memory 2" },
-    { src: "/src/assets/images/memories/m3.jpg", label: "Memory 3" },
-    { src: "/src/assets/images/memories/m4.jpg", label: "Memory 4" },
-    { src: "/src/assets/images/memories/m5.jpg", label: "Memory 5" },
-    { src: "/src/assets/images/memories/m6.jpg", label: "Memory 6" },
-    { src: "/src/assets/images/memories/m7.jpg", label: "Memory 7" },
-    { src: "/src/assets/images/memories/m8.jpg", label: "Memory 8" },
-    { src: "/src/assets/images/memories/m9.jpg", label: "Memory 9" },
-    { src: "/src/assets/images/memories/m10.jpg", label: "Memory 10" },
+    { src: "/images/memories/m1.jpg", label: "Memory 1" },
+    { src: "/images/memories/m2.jpg", label: "Memory 2" },
+    { src: "/images/memories/m3.jpg", label: "Memory 3" },
+    { src: "/images/memories/m4.jpg", label: "Memory 4" },
+    { src: "/images/memories/m5.jpg", label: "Memory 5" },
+    { src: "/images/memories/m6.jpg", label: "Memory 6" },
+    { src: "/images/memories/m7.jpg", label: "Memory 7" },
+    { src: "/images/memories/m8.jpg", label: "Memory 8" },
+    { src: "/images/memories/m9.jpg", label: "Memory 9" },
+    { src: "/images/memories/m10.jpg", label: "Memory 10" },
   ];
 
   return (
--- src/components/PersonalSlideshow.tsx
+++ src/components/PersonalSlideshow.tsx
@@ -3,16 +3,16 @@ import SmoothSlideshow from './SmoothSlideshow';
 
 const PersonalSlideshow: React.FC = () => {
   const personalImages = [
-    { src: "/src/assets/images/personal/av1.jpg", label: "Av #1" },
-    { src: "/src/assets/images/personal/av2.jpg", label: "Av #2" },
-    { src: "/src/assets/images/personal/av3.jpg", label: "Av #3" },
-    { src: "/src/assets/images/personal/av4.jpg", label: "Av #4" },
-    { src: "/src/assets/images/personal/av5.jpg", label: "Av #5" },
-    { src: "/src/assets/images/personal/av6.jpg", label: "Av #6" },
-    { src: "/src/assets/images/personal/av7.jpg", label: "Av #7" },
-    { src: "/src/assets/images/personal/av8.jpg", label: "Av #8" },
-    { src: "/src/assets/images/personal/av9.jpg", label: "Av #9" },
-    { src: "/src/assets/images/personal/av10.jpg", label: "Av #10" },
+    { src: "/images/personal/av1.jpg", label: "Av #1" },
+    { src: "/images/personal/av2.jpg", label: "Av #2" },
+    { src: "/images/personal/av3.jpg", label: "Av #3" },
+    { src: "/images/personal/av4.jpg", label: "Av #4" },
+    { src: "/images/personal/av5.jpg", label: "Av #5" },
+    { src: "/images/personal/av6.jpg", label: "Av #6" },
+    { src: "/images/personal/av7.jpg", label: "Av #7" },
+    { src: "/images/personal/av8.jpg", label: "Av #8" },
+    { src: "/images/personal/av9.jpg", label: "Av #9" },
+    { src: "/images/personal/av10.jpg", label: "Av #10" },
   ];
 
   return (
